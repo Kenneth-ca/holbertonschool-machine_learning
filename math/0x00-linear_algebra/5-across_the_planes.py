0d106f134f902178e20e722c0ea5350c3d0cba91
Kenneth-ca <kenneth.ca95@gmail.com>
2020-04-13 18:33:23 -0500

    [add] add two matrices elements

---
+++ math/0x00-linear_algebra/5-across_the_planes.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Module the sum of two matrices
+"""
+
+
+def add_matrices2D(mat1, mat2):
+    """
+    Needs a matrix as input
+    Returns the sum of two matrices a a list
+    """
+    if len(mat1) != len(mat2):
+        return None
+    if len(mat1[0]) != len(mat2[0]):
+        return None
+    return [[(mat1[i][j] + mat2[i][j]) for j in range(len(mat1[0]))] for i in range(len(mat1))]
