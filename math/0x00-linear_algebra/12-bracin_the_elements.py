b9d66bad36a8379c2491643bda424cfd1258ffa5
Kenneth-ca <kenneth.ca95@gmail.com>
2020-04-14 12:40:28 -0500

    [add] basic operations using matrices

---
+++ math/0x00-linear_algebra/12-bracin_the_elements.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Module that preforms aritmetic operations to matrices
+"""
+import numpy as np
+
+
+def np_elementwise(mat1, mat2):
+    """
+    Needs a matrix as input
+    Returns the resulting matrix
+    """
+    return (mat1 + mat2, mat1 - mat2, mat1 * mat2, mat1 / mat2)
---
+++ math/0x00-linear_algebra/12-main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import numpy as np
+np_elementwise = __import__('12-bracin_the_elements').np_elementwise
+
+mat1 = np.array([[11, 22, 33], [44, 55, 66]])
+mat2 = np.array([[1, 2, 3], [4, 5, 6]])
+
+print(mat1)
+print(mat2)
+add, sub, mul, div = np_elementwise(mat1, mat2)
+print("Add:\n", add, "\nSub:\n", sub, "\nMul:\n", mul, "\nDiv:\n", div)
+add, sub, mul, div = np_elementwise(mat1, 2)
+print("Add:\n", add, "\nSub:\n", sub, "\nMul:\n", mul, "\nDiv:\n", div)
