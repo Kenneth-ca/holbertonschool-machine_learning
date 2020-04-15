4b0257c1e655824ff1f844503d6ca02ed6010897
Kenneth-ca <kenneth.ca95@gmail.com>
2020-04-15 10:45:56 -0500

    [fix] aditional bracket in list

--- math/0x00-linear_algebra/7-gettin_cozy.py
+++ math/0x00-linear_algebra/7-gettin_cozy.py
@@ -9,10 +9,9 @@ def cat_matrices2D(mat1, mat2, axis=0):
     Needs a matrix as input
     Returns a list of matrices concatenated
     """
-    concat = []
     if (len(mat1[0]) == len(mat2[0])) and (axis == 0):
-        concat.append([ele.copy() for ele in mat1])
-        concat.append([ele.copy() for ele in mat2])
+        concat = [ele.copy() for ele in mat1]
+        concat += [ele.copy() for ele in mat2]
         return concat
     elif len(mat1) == len(mat2):
         concat = [mat1[j] + mat2[j] for j in range(len(mat1))]
