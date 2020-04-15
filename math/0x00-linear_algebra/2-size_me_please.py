a2aef0d29e99d9ac27d776825dd96ba28683bda3
Kenneth-ca <kenneth.ca95@gmail.com>
2020-04-15 12:49:33 -0500

    [feat] n dimensions with recursion

--- math/0x00-linear_algebra/2-size_me_please.py
+++ math/0x00-linear_algebra/2-size_me_please.py
@@ -3,25 +3,13 @@
 Module to calculate the shape of a matrix
 """
 
+
 def matrix_shape(matrix):
     """
     Needs a matrix as input
     Returns the shape as a list of integers
     """
-    integers = []
-    # Number of rows/elements
-    try:
-        integers.append(len(matrix))
-    except:
-        pass
-    # Number of columns/elements
-    try:
-        integers.append(len(matrix[0]))
-    except:
-        pass
-    # Number of elements
-    try:
-        integers.append(len(matrix[0][0]))
-    except:
-        pass
-    return integers
+    if type(matrix[0]) is not list:
+        return [len(matrix)]
+    else:
+        return [len(matrix)] + matrix_shape(matrix[0])
