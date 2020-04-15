8cd8eec653c8c734cdcb2cd161f674adc815242a
Kenneth-ca <kenneth.ca95@gmail.com>
2020-04-15 09:57:31 -0500

    pycodestyle

--- math/0x00-linear_algebra/5-across_the_planes.py
+++ math/0x00-linear_algebra/5-across_the_planes.py
@@ -13,4 +13,5 @@ def add_matrices2D(mat1, mat2):
         return None
     if len(mat1[0]) != len(mat2[0]):
         return None
-    return [[(mat1[i][j] + mat2[i][j]) for j in range(len(mat1[0]))] for i in range(len(mat1))]
+    return [[(mat1[i][j] + mat2[i][j]) for j in range(len(mat1[0]))]
+            for i in range(len(mat1))]
