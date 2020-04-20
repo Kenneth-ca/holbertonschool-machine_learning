887e5cb1904674700a1bb4ac1439e2dcf865992e
Kenneth-ca <kenneth.ca95@gmail.com>
2020-04-20 11:25:35 -0500

    Add scatter plot for Men's Height vs Weight

---
+++ math/0x01-plotting/1-scatter.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+import numpy as np
+import matplotlib.pyplot as plt
+
+mean = [69, 0]
+cov = [[15, 8], [8, 15]]
+np.random.seed(5)
+x, y = np.random.multivariate_normal(mean, cov, 2000).T
+y += 180
+
+plt.scatter(x, y, c='m')
+plt.title("Men's Height vs Weight")
+plt.xlabel('Height (in)')
+plt.ylabel('Weight (lbs)')
+plt.show()
