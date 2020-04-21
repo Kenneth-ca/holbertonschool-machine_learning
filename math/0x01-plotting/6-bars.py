9fd216f3f53820c2aae38be0075c233d0517b654
Kenneth-ca <kenneth.ca95@gmail.com>
2020-04-20 21:45:12 -0500

    Add stacked bar graph

---
+++ math/0x01-plotting/6-bars.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+import numpy as np
+import matplotlib.pyplot as plt
+
+np.random.seed(5)
+fruit = np.random.randint(0, 20, (4,3))
+
+names = ['Farrah', 'Fred', 'Felicia']
+fruits = ['apples', 'bananas', 'oranges', 'peaches']
+colors = ['red', 'yellow', '#ff8000', '#ffe5b4']
+
+for i in range(len(fruit)):
+    plt.bar(names, fruit[i], bottom=np.sum(fruit[:i], axis=0), color=colors[i], label=fruits[i], width=0.5)
+
+plt.title("Number of Fruit per Person")
+plt.ylabel("Quantity of Fruit")
+plt.yticks(np.arange(0, 90, 10))
+plt.legend()
+plt.show()
