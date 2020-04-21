9e6497e06b00e4dfa3d464c9ec57b3e0b6a76d13
Kenneth-ca <kenneth.ca95@gmail.com>
2020-04-20 23:09:54 -0500

    fix - pycodestyle

--- math/0x01-plotting/6-bars.py
+++ math/0x01-plotting/6-bars.py
@@ -10,7 +10,8 @@ fs = ['apples', 'bananas', 'oranges', 'peaches']
 colors = ['red', 'yellow', '#ff8000', '#ffe5b4']
 
 for i in range(len(fruit)):
-    plt.bar(names, fruit[i], bottom=np.sum(fruit[:i], axis=0), color=colors[i], label=fs[i], width=0.5)
+    plt.bar(names, fruit[i], bottom=np.sum(fruit[:i], axis=0),
+            color=colors[i], label=fs[i], width=0.5)
 
 plt.title("Number of Fruit per Person")
 plt.ylabel("Quantity of Fruit")
