e3796c744c5c40eca88994fe134f1284652ee3de
Kenneth-ca <kenneth.ca95@gmail.com>
2020-04-20 19:42:39 -0500

    fix - x and y limits

--- math/0x01-plotting/4-frequency.py
+++ math/0x01-plotting/4-frequency.py
@@ -5,8 +5,12 @@ import matplotlib.pyplot as plt
 np.random.seed(5)
 student_grades = np.random.normal(68, 15, 50)
 
-plt.hist(student_grades, bins=10, range=(0, 100), edgecolor='black')
+bins = np.arange(0, 110, 10)
+plt.hist(student_grades, bins=bins, edgecolor='black')
 plt.title('Project A')
+plt.xlim(0, 100)
+plt.xticks(np.arange(0, 110, 10))
 plt.xlabel('Grades')
+plt.ylim(0, 30)
 plt.ylabel('Number of Students')
 plt.show()
