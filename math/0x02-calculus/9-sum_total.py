6c21b0b9deef48de1fd16e7f4d081f1a39e49866
Kenneth-ca <kenneth.ca95@gmail.com>
2020-04-25 17:27:26 -0500

    fix - pycodestyle and return none for n < 1

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -11,12 +11,10 @@ def summation_i_squared(n):
     >>> print(summation_i_squared(n))
     55
     """
-    if (type(n) is not int) or (n is None):
+    if (type(n) is not int) or (n is None) or (n < 1):
         return None
-    elif n < 1:
-        return 0
     else:
         numbers = range(1, n + 1)
         result = 0
-        result = map(lambda i : i ** 2, numbers)
+        result = map(lambda i: i ** 2, numbers)
         return sum(result)
