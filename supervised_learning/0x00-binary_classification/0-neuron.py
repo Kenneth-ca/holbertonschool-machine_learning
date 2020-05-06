386cf769da99e31523a3346e93de3781f525d633
Kenneth-ca <kenneth.ca95@gmail.com>
2020-05-05 21:38:32 -0500

    fix - use nx value in w creation

--- supervised_learning/0x00-binary_classification/0-neuron.py
+++ supervised_learning/0x00-binary_classification/0-neuron.py
@@ -9,9 +9,6 @@ class Neuron:
     """
     A class that defines a single neuron
     """
-    W = np.random.randn(1, 784)
-    b = 0
-    A = 0
 
     def __init__(self, nx):
         """
@@ -22,3 +19,7 @@ class Neuron:
             raise TypeError("nx must be an integer")
         if nx < 1:
             raise ValueError("nx must be a positive integer")
+        self.nx = nx
+        self.W = np.random.randn(1, nx)
+        self.b = 0
+        self.A = 0
