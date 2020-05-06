ccb41adab1739b98cee1b4312a8e8cb92c7327fc
Kenneth-ca <kenneth.ca95@gmail.com>
2020-05-05 21:47:08 -0500

    add - neuron evaluation

---
+++ supervised_learning/0x00-binary_classification/4-main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import numpy as np
+
+Neuron = __import__('4-neuron').Neuron
+
+lib_train = np.load('../data/Binary_Train.npz')
+X_3D, Y = lib_train['X'], lib_train['Y']
+X = X_3D.reshape((X_3D.shape[0], -1)).T
+
+np.random.seed(0)
+neuron = Neuron(X.shape[0])
+A, cost = neuron.evaluate(X, Y)
+print(A)
+print(cost)
---
+++ supervised_learning/0x00-binary_classification/4-neuron.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python3
+"""
+Module to create a neuron
+"""
+import numpy as np
+
+
+class Neuron:
+    """
+    A class that defines a single neuron
+    """
+
+    def __init__(self, nx):
+        """
+        class constructor
+        :param nx: is the number of input features to the neuron
+        """
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        self.nx = nx
+        self.__W = np.random.randn(1, nx)
+        self.__b = 0
+        self.__A = 0
+
+    def forward_prop(self, X):
+        """
+        calculates the forward propagation of the neuron
+        :param X: a np array with shape (nx, m) that contains the input data
+        :return: private attribute __A
+        """
+        preactivation = np.matmul(self.__W, X) + self.__b
+        self.__A = 1 / (1 + np.exp(-preactivation))
+        return self.__A
+
+    def cost(self, Y, A):
+        """
+        calculates the cost of the model using logistic regression
+        :param Y: a np array with shape (1, m) with correct labels
+        :param A: a np array with shape (1, m) containing the activated output
+        :return: the cost
+        """
+        cost = Y * np.log(A) + (1 - Y) * np.log(1.0000001 - A)
+        cost = np.sum(cost)
+        cost = - cost / A.shape[1]
+        return cost
+
+    def evaluate(self, X, Y):
+        """
+        evaluates the neuron prediction
+        :param X: np array with input data and shape (nx, m)
+        :param Y: np array with correct label and shape (1, m)
+        :return: neuron´s prediction and cost of the network
+        """
+        self.forward_prop(X)
+        prediction = np.where(self.__A >= 0.5, 1, 0)
+        cost = self.cost(Y, self.__A)
+        return prediction, cost
+
+    @property
+    def W(self):
+        """
+        getter function for W
+        :return: weight vector neuron
+        """
+        return self.__W
+
+    @property
+    def b(self):
+        """
+        getter function for b
+        :return: bias for neuron
+        """
+        return self.__b
+
+    @property
+    def A(self):
+        """
+        getter function for W
+        :return: activated output of the neuron
+        """
+        return self.__A
