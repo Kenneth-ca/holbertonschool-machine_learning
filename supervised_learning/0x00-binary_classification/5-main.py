1b88e303ab303e65366a845d72ab3e3c34e053f8
Kenneth-ca <kenneth.ca95@gmail.com>
2020-05-05 21:47:21 -0500

    add - neuron gradient descent

---
+++ supervised_learning/0x00-binary_classification/5-main.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import numpy as np
+
+Neuron = __import__('5-neuron').Neuron
+
+lib_train = np.load('../data/Binary_Train.npz')
+X_3D, Y = lib_train['X'], lib_train['Y']
+X = X_3D.reshape((X_3D.shape[0], -1)).T
+
+np.random.seed(0)
+neuron = Neuron(X.shape[0])
+A = neuron.forward_prop(X)
+neuron.gradient_descent(X, Y, A, 0.5)
+print(neuron.W)
+print(neuron.b)
