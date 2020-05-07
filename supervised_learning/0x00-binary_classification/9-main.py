6b911ad639c92ddb163bdcdb17450c914321d183
Kenneth-ca <kenneth.ca95@gmail.com>
2020-05-07 13:28:33 -0500

    add - privatising class attributes

---
+++ supervised_learning/0x00-binary_classification/9-main.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import numpy as np
+
+NN = __import__('9-neural_network').NeuralNetwork
+
+lib_train = np.load('../data/Binary_Train.npz')
+X_3D, Y = lib_train['X'], lib_train['Y']
+X = X_3D.reshape((X_3D.shape[0], -1)).T
+
+np.random.seed(0)
+nn = NN(X.shape[0], 3)
+print(nn.W1)
+print(nn.b1)
+print(nn.W2)
+print(nn.b2)
+print(nn.A1)
+print(nn.A2)
+nn.A1 = 10
+print(nn.A1)
---
+++ supervised_learning/0x00-binary_classification/9-neural_network.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+"""
+Module to create a neural network
+"""
+import numpy as np
+
+
+class NeuralNetwork:
+    """
+    A class that defines a neural network with one hidden layer performing
+    binary classification
+    """
+
+    def __init__(self, nx, nodes):
+        """
+        class constructor
+        :param nx: is the number of input features to the neuron
+        """
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        self.nx = nx
+        if type(nodes) is not int:
+            raise TypeError("nodes must be an integer")
+        if nodes < 1:
+            raise ValueError("nodes must be a positive integer")
+        # hidden layer
+        self.__W1 = np.random.randn(nodes, nx)
+        self.__b1 = np.zeros((nodes, 1))
+        self.__A1 = 0
+        # output neuron
+        self.__W2 = np.random.randn(1, nodes)
+        self.__b2 = 0
+        self.__A2 = 0
+
+    @property
+    def W1(self):
+        return self.__W1
+
+    @property
+    def b1(self):
+        return self.__b1
+
+    @property
+    def A1(self):
+        return self.__A1
+
+    @property
+    def W2(self):
+        return self.__W2
+
+    @property
+    def b2(self):
+        return self.__b2
+
+    @property
+    def A2(self):
+        return self.__A2
