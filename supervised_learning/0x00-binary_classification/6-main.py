8c7022e573c4520b2fc055c6f6840bc23f3c6076
Kenneth-ca <kenneth.ca95@gmail.com>
2020-05-06 23:16:48 -0500

    add - training of the neuron

---
+++ supervised_learning/0x00-binary_classification/6-main.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+Neuron = __import__('6-neuron').Neuron
+
+lib_train = np.load('../data/Binary_Train.npz')
+X_train_3D, Y_train = lib_train['X'], lib_train['Y']
+X_train = X_train_3D.reshape((X_train_3D.shape[0], -1)).T
+lib_dev = np.load('../data/Binary_Dev.npz')
+X_dev_3D, Y_dev = lib_dev['X'], lib_dev['Y']
+X_dev = X_dev_3D.reshape((X_dev_3D.shape[0], -1)).T
+
+np.random.seed(0)
+neuron = Neuron(X_train.shape[0])
+A, cost = neuron.train(X_train, Y_train, iterations=10)
+accuracy = np.sum(A == Y_train) / Y_train.shape[1] * 100
+print("Train cost:", cost)
+print("Train accuracy: {}%".format(accuracy))
+A, cost = neuron.evaluate(X_dev, Y_dev)
+accuracy = np.sum(A == Y_dev) / Y_dev.shape[1] * 100
+print("Dev cost:", cost)
+print("Dev accuracy: {}%".format(accuracy))
+fig = plt.figure(figsize=(10, 10))
+for i in range(100):
+    fig.add_subplot(10, 10, i + 1)
+    plt.imshow(X_dev_3D[i])
+    plt.title(A[0, i])
+    plt.axis('off')
+plt.tight_layout()
+plt.show()
