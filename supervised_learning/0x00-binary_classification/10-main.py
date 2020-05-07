51dba0ad976d1a22bbaee25f7ce214c5e31f29ab
Kenneth-ca <kenneth.ca95@gmail.com>
2020-05-07 13:43:32 -0500

    add - forward propagation of the neural network

---
+++ supervised_learning/0x00-binary_classification/10-main.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import numpy as np
+
+NN = __import__('10-neural_network').NeuralNetwork
+
+lib_train = np.load('../data/Binary_Train.npz')
+X_3D, Y = lib_train['X'], lib_train['Y']
+X = X_3D.reshape((X_3D.shape[0], -1)).T
+
+np.random.seed(0)
+nn = NN(X.shape[0], 3)
+nn._NeuralNetwork__b1 = np.ones((3, 1))
+nn._NeuralNetwork__b2 = 1
+A1, A2 = nn.forward_prop(X)
+if A1 is nn.A1:
+        print(A1)
+if A2 is nn.A2:
+        print(A2)
