f7f6388309d7a22bff00fc52230481be5b5894b5
Kenneth-ca <kenneth.ca95@gmail.com>
2020-05-08 17:37:20 -0500

    add - getter attributes

---
+++ supervised_learning/0x00-binary_classification/17-main.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import numpy as np
+
+Deep = __import__('17-deep_neural_network').DeepNeuralNetwork
+
+lib_train = np.load('../data/Binary_Train.npz')
+X_3D, Y = lib_train['X'], lib_train['Y']
+X = X_3D.reshape((X_3D.shape[0], -1)).T
+
+np.random.seed(0)
+deep = Deep(X.shape[0], [5, 3, 1])
+print(deep.cache)
+print(deep.weights)
+print(deep.L)
+deep.L = 10
+print(deep.L)
