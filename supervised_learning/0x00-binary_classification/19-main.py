a845d86dadb88bae918054b2357fd320ef07fa24
Kenneth-ca <kenneth.ca95@gmail.com>
2020-05-08 19:47:24 -0500

    add - cost of the deep neural network

---
+++ supervised_learning/0x00-binary_classification/19-main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import numpy as np
+
+Deep = __import__('19-deep_neural_network').DeepNeuralNetwork
+
+lib_train = np.load('../data/Binary_Train.npz')
+X_3D, Y = lib_train['X'], lib_train['Y']
+X = X_3D.reshape((X_3D.shape[0], -1)).T
+
+np.random.seed(0)
+deep = Deep(X.shape[0], [5, 3, 1])
+A, _ = deep.forward_prop(X)
+cost = deep.cost(Y, A)
+print(cost)
