eab06a13c1d7ff83daa7b0ab2c923b744055a4ed
Kenneth-ca <kenneth.ca95@gmail.com>
2020-05-11 13:08:31 -0500

    add - one-hot-matrix decode

---
+++ supervised_learning/0x01-multiclass_classification/1-main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import numpy as np
+
+oh_encode = __import__('0-one_hot_encode').one_hot_encode
+oh_decode = __import__('1-one_hot_decode').one_hot_decode
+
+lib = np.load('../data/MNIST.npz')
+Y = lib['Y_train'][:10]
+
+print(Y)
+Y_one_hot = oh_encode(Y, 10)
+Y_decoded = oh_decode(Y_one_hot)
+print(Y_decoded)
---
+++ supervised_learning/0x01-multiclass_classification/1-one_hot_decode.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Converts a one-hot matrix into a numeric label vector:
+"""
+import numpy as np
+
+
+def one_hot_decode(one_hot):
+    """
+    converts to a vector of labels
+    :param one_hot: a one-hot encoded numpy.ndarray with shape (classes, m)
+    :return: a numpy.ndarray with shape (m, ) containing the numeric labels
+    for each example, or None on failure
+    """
+    if type(one_hot) is not np.ndarray:
+        return None
+    labels = np.argmax(one_hot, axis=1)
+    return labels
