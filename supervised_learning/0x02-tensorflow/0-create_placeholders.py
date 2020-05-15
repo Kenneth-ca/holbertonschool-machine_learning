bf674fa0d934ff67b78ccd6204f5c4f3d1a3ea39
Kenneth-ca <kenneth.ca95@gmail.com>
2020-05-15 21:01:24 +0000

    add - creation of placeholders

---
+++ supervised_learning/0x02-tensorflow/0-create_placeholders.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Module to create a placeholder
+"""
+import tensorflow as tf
+
+
+def create_placeholders(nx, classes):
+    """
+    a function that create placeholders
+    :param nx: the number of feature columns in our data
+    :param classes: the number of classes in our classifier
+    :return: placeholders named x and y, respectively
+    """
+    return tf.placeholder(float, shape=[None, nx], name='x'), tf.placeholder(
+        float, shape=[None,classes], name='y')
