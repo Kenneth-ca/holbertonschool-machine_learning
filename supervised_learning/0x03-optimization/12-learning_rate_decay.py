4b72e3ac4b5093aea13ae4870e57f7e309bb8260
Kenneth-ca <kenneth.ca95@gmail.com>
2020-05-22 19:10:42 -0500

    fix - staircase True apply discrete staircase

--- supervised_learning/0x03-optimization/12-learning_rate_decay.py
+++ supervised_learning/0x03-optimization/12-learning_rate_decay.py
@@ -17,5 +17,7 @@ def learning_rate_decay(alpha, decay_rate, global_step, decay_step):
     occur before alpha is decayed further
     :return: the learning rate decay operation
     """
+    # staircase= True will make discrete output as floor(global_step /
+    # decay_step in the calculation)
     return tf.train.inverse_time_decay(alpha, global_step, decay_step,
-                                       decay_rate)
+                                       decay_rate, staircase=True)
