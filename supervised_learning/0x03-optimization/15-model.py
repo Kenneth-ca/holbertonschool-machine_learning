ef3e5994ae16d9f8d1f368229278853586b3b131
Kenneth-ca <kenneth.ca95@gmail.com>
2020-05-24 22:46:04 -0500

    fix - update of global step for learning rate

--- supervised_learning/0x03-optimization/15-model.py
+++ supervised_learning/0x03-optimization/15-model.py
@@ -158,7 +158,7 @@ def model(Data_train, Data_valid, layers, activations, alpha=0.001,
     labels, respectively
     :param Data_valid: tuple containing the validation inputs and validation
     labels, respectively
-    :param layers: a list containing the number of nodes in each layer of the networ
+    :param layers: a list containing the number of nodes in each layer
     :param activations: a list containing the activation functions used for
     each layer of the network
     :param alpha: the learning rate
@@ -247,5 +247,8 @@ def model(Data_train, Data_valid, layers, activations, alpha=0.001,
                         print("\tStep {}:".format(b + 1))
                         print("\t\tCost: {}".format(loss_mini_batch))
                         print("\t\tAccuracy: {}".format(acc_mini_batch))
+
+            # Update of global step variable for each iteration
+            sess.run(tf.assign(global_step, global_step + 1))
 
         return saver.save(sess, save_path)
