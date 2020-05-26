67b780b20a3dc3c78702e8c378e96f3b3ffe9cb4
Kenneth-ca <kenneth.ca95@gmail.com>
2020-05-26 12:59:46 -0500

    add - dropout implementation with inverse dropout

---
+++ supervised_learning/0x05-regularization/4-dropout_forward_prop.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+"""
+Conducts forward propagation using Dropout:
+"""
+import numpy as np
+
+
+def dropout_forward_prop(X, weights, L, keep_prob):
+    """
+    a function that cnducts forward porpagation using dropout
+    :param X: a numpy.ndarray of shape (nx, m) containing the input data for
+    the network
+        nx is the number of input features
+        m is the number of data points
+    :param weights: a dictionary of the weights and biases of the neural
+    network
+    :param L: the number of layers in the network
+    :param keep_prob: the probability that a node will be kept
+    :return: a dictionary containing the outputs of each layer and the
+    dropout mask used on each layer (see example for format)
+    """
+    cache = {'A0': X}
+    dropout = {'A0': X}
+    # Hidden and output layer
+    for i in range(L):
+        # create keys to access weights(W), biases(b) and store in cache
+        key_w = 'W' + str(i + 1)
+        key_b = 'b' + str(i + 1)
+        key_cache = 'A' + str(i + 1)
+        key_cache_last = 'A' + str(i)
+        # store activation in cache
+        output_Z = np.matmul(weights[key_w], cache[
+            key_cache_last]) + weights[key_b]
+        if i == L - 1:
+            # Softmax
+            t = np.exp(output_Z)
+            output_A = np.exp(output_Z) / np.sum(t, axis=0,
+                                                 keepdims=True)
+        else:
+            output_A = ((np.exp(output_Z) - np.exp(-output_Z)) / (
+                np.exp(output_Z) + np.exp(-output_Z)))
+            # dropout value is 0 if random number less than keep_prop
+            rand = np.random.rand(output_A.shape[0], output_A.shape[1])
+            d = (rand < keep_prob).astype(int)
+            # dropout of the neuron if multiply by 0
+            output_A = np.multiply(output_A, d)
+            output_A = output_A / keep_prob
+            dropout['D{}'.format(i + 1)] = d
+        cache[key_cache] = output_A
+        dropout[key_cache] = output_A
+    return dropout
---
+++ supervised_learning/0x05-regularization/4-main.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+import numpy as np
+dropout_forward_prop = __import__('4-dropout_forward_prop').dropout_forward_prop
+
+
+def one_hot(Y, classes):
+    """convert an array to a one-hot matrix"""
+    m = Y.shape[0]
+    one_hot = np.zeros((classes, m))
+    one_hot[Y, np.arange(m)] = 1
+    return one_hot
+
+if __name__ == '__main__':
+    lib= np.load('../data/MNIST.npz')
+    X_train_3D = lib['X_train']
+    Y_train = lib['Y_train']
+    X_train = X_train_3D.reshape((X_train_3D.shape[0], -1)).T
+    Y_train_oh = one_hot(Y_train, 10)
+
+    np.random.seed(0)
+
+    weights = {}
+    weights['W1'] = np.random.randn(256, 784)
+    weights['b1'] = np.zeros((256, 1))
+    weights['W2'] = np.random.randn(128, 256)
+    weights['b2'] = np.zeros((128, 1))
+    weights['W3'] = np.random.randn(10, 128)
+    weights['b3'] = np.zeros((10, 1))
+
+    cache = dropout_forward_prop(X_train, weights, 3, 0.8)
+    for k, v in sorted(cache.items()):
+        print(k, v)
