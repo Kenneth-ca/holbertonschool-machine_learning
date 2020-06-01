999c17c29b3cf4413d196a34aae784d1279438cb
Kenneth-ca <kenneth.ca95@gmail.com>
2020-06-01 17:36:51 -0500

    add - update of train model with learning rate decay

---
+++ supervised_learning/0x06-keras/7-main.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+import numpy as np
+import tensorflow as tf
+build_model = __import__('1-input').build_model
+optimize_model = __import__('2-optimize').optimize_model
+one_hot = __import__('3-one_hot').one_hot
+train_model = __import__('7-train').train_model
+
+
+if __name__ == '__main__':
+    datasets = np.load('../data/MNIST.npz')
+    X_train = datasets['X_train']
+    X_train = X_train.reshape(X_train.shape[0], -1)
+    Y_train = datasets['Y_train']
+    Y_train_oh = one_hot(Y_train)
+    X_valid = datasets['X_valid']
+    X_valid = X_valid.reshape(X_valid.shape[0], -1)
+    Y_valid = datasets['Y_valid']
+    Y_valid_oh = one_hot(Y_valid)
+
+    np.random.seed(0)
+    tf.set_random_seed(0)
+    lambtha = 0.0001
+    keep_prob = 0.95
+    network = build_model(784, [256, 256, 10], ['relu', 'relu', 'softmax'], lambtha, keep_prob)
+    alpha = 0.001
+    beta1 = 0.9
+    beta2 = 0.999
+    optimize_model(network, alpha, beta1, beta2)
+    batch_size = 64
+    epochs = 1000
+    train_model(network, X_train, Y_train_oh, batch_size, epochs,
+                validation_data=(X_valid, Y_valid_oh), early_stopping=True,
+                patience=3, learning_rate_decay=True, alpha=alpha)
