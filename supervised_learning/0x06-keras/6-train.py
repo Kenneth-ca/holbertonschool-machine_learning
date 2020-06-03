50ac7dab08ca957d0c5913663a5fda10f3719978
Kenneth-ca <kenneth.ca95@gmail.com>
2020-06-02 20:21:02 -0500

    feat - more testing

--- supervised_learning/0x06-keras/6-train.py
+++ supervised_learning/0x06-keras/6-train.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
-"""
-Updates the function train model with early stop
-"""
+"""Updates train model with early stopping"""
 import tensorflow.keras as K
 
 
@@ -9,29 +7,28 @@ def train_model(network, data, labels, batch_size, epochs,
                 validation_data=None, early_stopping=False, patience=0,
                 verbose=True, shuffle=False):
     """
-    a function that trains a model using mini-batch gradient descent
-    :param network: the model to train
-    :param data: numpy.ndarray of shape (m, nx) containing the input data
-    :param labels: a one-hot numpy.ndarray of shape (m, classes) containing
-    the labels of data
-    :param batch_size: the size of the batch used for mini-batch gradient
-    descent
-    :param epochs: the number of passes through data for mini-batch gradient
-    descent
-    :param validation_data: data to validate the model with
+    a function that trains the model using early stopping
+    :param network: is the model to train
+    :param data: ndarray of shape (m, nx) containing the input data
+    :param labels: is a one-hot numpy.ndarray of shape (m, classes)
+    containing the labels of data
+    :param batch_size: size of the batch used for mini-batch gradient descent
+    :param epochs:number of passes through data for mini-batch gradient descent
+    :param validation_data: is the data to validate the model with, if not None
     :param early_stopping: indicates whether early stopping should be used
-    :param patience: the patience used for early stopping
-    :param verbose: a boolean that determines if output should be printed
-    during training
-    :param shuffle: a boolean that determines whether to shuffle the batches
+    early stopping should only be performed if validation_data exists
+    early stopping should be based on validation loss
+    :param patience: is the patience used for early stopping
+    :param verbose: boolean that determines if output should be printed
+    :param shuffle: boolean that determines whether to shuffle the batches
     every epoch. Normally, it is a good idea to shuffle, but for
-    reproducibility, we have chosen to set the default to False
-    :return: the History object generated after training the model
+    reproducibility, we have chosen to set the default to False.
+    :return:
     """
-    callback = []
+    stopping = []
     if early_stopping and validation_data:
-        callback.append(K.callbacks.EarlyStopping(patience=patience))
+        stopping.append(K.callbacks.EarlyStopping(patience=patience))
     history = network.fit(data, labels, epochs=epochs, batch_size=batch_size,
-                          shuffle=shuffle, verbose=verbose, callbacks=callback,
+                          shuffle=shuffle, verbose=verbose, callbacks=stopping,
                           validation_data=validation_data)
     return history
