8e7cbe99e757a7e14e26d00d40f8bd00a09af566
Kenneth-ca <kenneth.ca95@gmail.com>
2020-06-04 17:27:22 -0500

    add - valid convolution

---
+++ math/0x04-convolutions_and_pooling/0-convolve_grayscale_valid.py
@@ -0,0 +1,30 @@
+"""
+Performs a valid convolution on grayscale images
+"""
+import numpy as np
+
+
+def convolve_grayscale_valid(images, kernel):
+    """
+    a function that performs a convolution
+    :param images: a numpy.ndarray with shape (m, h, w) containing multiple
+    grayscale images
+        m is the number of images
+        h is the height in pixels of the images
+        w is the width in pixels of the images
+    :param kernel: a numpy.ndarray with shape (kh, kw) containing the kernel
+    for the convolution
+        kh is the height of the kernel
+        kw is the width of the kernel
+    :return: numpy.ndarray containing the convolved images
+    """
+    c_images = images.shape[0]
+    c_height = images.shape[1] - kernel.shape[0] + 1
+    c_width = images.shape[2] - kernel.shape[1] + 1
+    convolved = np.zeros((c_images, c_height, c_width))
+    for row in range(c_height):
+        for col in range(c_width):
+            element = np.sum(images[:, row:row+3, col:col+3] * kernel,
+                             axis=(1, 2))
+            convolved[:, row, col] = element
+    return convolved
---
+++ math/0x04-convolutions_and_pooling/0-main.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import matplotlib.pyplot as plt
+import numpy as np
+convolve_grayscale_valid = __import__('0-convolve_grayscale_valid').convolve_grayscale_valid
+
+
+if __name__ == '__main__':
+
+    dataset = np.load('../../supervised_learning/data/MNIST.npz')
+    images = dataset['X_train']
+    print(images.shape)
+    kernel = np.array([[1 ,0, -1], [1, 0, -1], [1, 0, -1]])
+    images_conv = convolve_grayscale_valid(images, kernel)
+    print(images_conv.shape)
+
+    plt.imshow(images[0], cmap='gray')
+    plt.show()
+    plt.imshow(images_conv[0], cmap='gray')
+    plt.show()
+    plt.imshow(images_conv[1], cmap='gray')
+    plt.show()
+    plt.imshow(images_conv[1000], cmap='gray')
+    plt.show()
