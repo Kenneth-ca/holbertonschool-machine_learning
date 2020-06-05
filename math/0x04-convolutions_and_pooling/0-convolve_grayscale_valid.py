03ee9ff1f0cb2c3e208bc382d7328549b76bb09e
Kenneth-ca <kenneth.ca95@gmail.com>
2020-06-04 23:06:50 -0500

    fix - filter height and width

--- math/0x04-convolutions_and_pooling/0-convolve_grayscale_valid.py
+++ math/0x04-convolutions_and_pooling/0-convolve_grayscale_valid.py
@@ -20,12 +20,15 @@ def convolve_grayscale_valid(images, kernel):
     :return: numpy.ndarray containing the convolved images
     """
     c_images = images.shape[0]
-    c_height = images.shape[1] - kernel.shape[0] + 1
-    c_width = images.shape[2] - kernel.shape[1] + 1
+    f_height = kernel.shape[0]
+    f_width = kernel.shape[1]
+    c_height = images.shape[1] - f_height + 1
+    c_width = images.shape[2] - f_width + 1
+
     convolved = np.zeros((c_images, c_height, c_width))
     for row in range(c_height):
         for col in range(c_width):
-            element = np.sum(images[:, row:row+3, col:col+3] * kernel,
-                             axis=(1, 2))
-            convolved[:, row, col] = element
+            mul_ele = images[:, row:row + f_height, col:col + f_width] * kernel
+            sum_ele = np.sum(mul_ele, axis=(1, 2))
+            convolved[:, row, col] = sum_ele
     return convolved
--- math/0x04-convolutions_and_pooling/1-convolve_grayscale_same.py
+++ math/0x04-convolutions_and_pooling/1-convolve_grayscale_same.py
@@ -22,8 +22,10 @@ def convolve_grayscale_same(images, kernel):
     padding_h = (kernel.shape[0] - 1) // 2
     padding_w = (kernel.shape[1] - 1) // 2
     c_images = images.shape[0]
-    c_height = images.shape[1] + 2 * padding_h - kernel.shape[0] + 1
-    c_width = images.shape[2] + 2 * padding_w - kernel.shape[1] + 1
+    f_height = kernel.shape[0]
+    f_width = kernel.shape[1]
+    c_height = images.shape[1] + 2 * padding_h - f_height + 1
+    c_width = images.shape[2] + 2 * padding_w - f_width + 1
     # np.pad works with a before_N and after_N parameter defined in a tuple
     # that will add the selected pad at each dimension
     pad_images = np.pad(images, ((0, 0), (padding_h, padding_h), (padding_w,
@@ -31,7 +33,8 @@ def convolve_grayscale_same(images, kernel):
     convolved = np.zeros((c_images, c_height, c_width))
     for row in range(c_height):
         for col in range(c_width):
-            element = np.sum(pad_images[:, row:row+3, col:col+3] * kernel,
-                             axis=(1, 2))
-            convolved[:, row, col] = element
+            mul_ele = pad_images[:, row:row + f_height, col:col + f_width] * \
+                      kernel
+            sum_ele = np.sum(mul_ele, axis=(1, 2))
+            convolved[:, row, col] = sum_ele
     return convolved
