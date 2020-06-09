aad276a6a2ea5927bbbe7e8c933d68b207882abc
Kenneth-ca <kenneth.ca95@gmail.com>
2020-06-08 19:11:11 -0500

    add - forward propagation with pooling layer

---
+++ supervised_learning/0x07-cnn/1-main.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import matplotlib.pyplot as plt
+import numpy as np
+pool_forward = __import__('1-pool_forward').pool_forward
+
+if __name__ == "__main__":
+    np.random.seed(0)
+    lib = np.load('../data/MNIST.npz')
+    X_train = lib['X_train']
+    m, h, w = X_train.shape
+    X_train_a = X_train.reshape((-1, h, w, 1))
+    X_train_b = 1 - X_train_a
+    X_train_c = np.concatenate((X_train_a, X_train_b), axis=3)
+
+    print(X_train_c.shape)
+    plt.imshow(X_train_c[0, :, :, 0])
+    plt.show()
+    plt.imshow(X_train_c[0, :, :, 1])
+    plt.show()
+    A = pool_forward(X_train_c, (2, 2), stride=(2, 2))
+    print(A.shape)
+    plt.imshow(A[0, :, :, 0])
+    plt.show()
+    plt.imshow(A[0, :, :, 1])
+    plt.show()
---
+++ supervised_learning/0x07-cnn/1-pool_forward.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""
+Performs forward propagation over a convolutional layer of a neural network
+"""
+import numpy as np
+
+
+def pool_forward(A_prev, kernel_shape, stride=(1, 1), mode='max'):
+    """
+    a function that performs forward propagation over a CNN
+    :param A_prev: numpy.ndarray of shape (m, h_prev, w_prev, c_prev)
+    containing the output of the previous layer
+    :param kernel_shape: a tuple of (kh, kw) containing the size of the kernel
+    for the pooling
+    :param stride: is a tuple of (sh, sw) containing the strides for the
+    convolution
+    :param mode: a string containing either max or avg, indicating whether to
+    perform maximum or average pooling, respectively
+    :return: the output of the convolutional layer
+    """
+    m, h_prev, w_prev, c_prev = A_prev.shape
+    kh, kw = kernel_shape
+    sh, sw = stride
+
+    pool_h = (h_prev - kh) // sh + 1
+    pool_w = (w_prev - kw) // sw + 1
+
+    pooled = np.zeros((m, pool_h, pool_w, c_prev))
+    # In this notation row refers to height and col to width
+    for row in range(pool_h):
+        for col in range(pool_w):
+            slice_A = A_prev[:, row * sh:row * sh + kh, col * sh:col * sw + kh]
+            if mode == "max":
+                pooled[:, row, col] = np.max(slice_A, axis=(1, 2))
+            if mode == "avg":
+                pooled[:, row, col] = np.mean(slice_A, axis=(1, 2))
+    return pooled
