a09f40adb89257513785852bc588f7b672b363e3
Kenneth-ca <kenneth.ca95@gmail.com>
2020-06-08 20:44:43 -0500

    add - backward propagation with convolutional layer

---
+++ supervised_learning/0x07-cnn/2-conv_backward.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+"""
+Performs forward propagation over a convolutional layer of a neural network
+"""
+import numpy as np
+
+
+def conv_backward(dZ, A_prev, W, b, padding="same", stride=(1, 1)):
+    """
+    a function that performs forward propagation over a CNN
+    :param dZ: is a numpy.ndarray of shape (m, h_new, w_new, c_new) containing
+    the partial derivatives with respect to the unactivated output of the
+    convolutional layer
+    :param A_prev: numpy.ndarray of shape (m, h_prev, w_prev, c_prev)
+    containing the output of the previous layer
+    :param W: numpy.ndarray of shape (kh, kw, c_prev, c_new) containing the
+    kernels for the convolution
+    :param b: numpy.ndarray of shape (1, 1, 1, c_new) containing the biases
+    applied to the convolution
+    :param padding:  is a string that is either same or valid, indicating the
+    type of padding used
+    :param stride: is a tuple of (sh, sw) containing the strides for the
+    convolution
+    :return: the partial derivatives with respect to the previous layer (
+    dA_prev), the kernels (dW), and the biases (db), respectively
+    """
+    m, h_prev, w_prev, c_prev = A_prev.shape
+    m, h_new, w_new, c_new = dZ.shape
+    kh, kw, c_prev, c_new = W.shape
+    sh, sw = stride
+
+    pad_h, pad_w = (0, 0)
+    if padding == "same":
+        pad_h = ((h_prev - 1) * sh + kw - h_prev) // 2 + 1
+        pad_w = ((w_prev - 1) * sh + kw - w_prev) // 2 + 1
+
+    dA = np.zeros(A_prev.shape)
+    dW = np.zeros(W.shape)
+    db = np.zeros(b.shape)
+
+    A_pad = np.pad(A_prev, ((0, 0), (pad_h, pad_h), (pad_w, pad_w), (0, 0)),
+                   mode="constant", constant_values=(0, 0))
+
+    dA_pad = np.pad(A_prev, ((0, 0), (pad_h, pad_h), (pad_w, pad_w), (0, 0)),
+                    mode="constant", constant_values=(0, 0))
+
+    # In this notation row refers to height and col to width
+    for img in range(m):
+        A_img = A_pad[img]
+        dA_img = dA_pad[img]
+        for row in range(h_new):
+            for col in range(w_new):
+                for ch in range(c_new):
+                    # corners of the slice
+                    row_start = row * sh
+                    row_end = row * sh + kh
+                    col_start = col * sw
+                    col_end = col * sw + kw
+
+                    slice_A = A_img[row_start:row_end, col_start:col_end, :]
+                    aux = W[:, :, :, ch] * dZ[img, row, col, ch]
+                    dA_img[row_start:row_end, col_start:col_end] += aux
+                    dW[:, :, :, ch] += slice_A * dZ[img, row, col, ch]
+                    db[:, :, :, ch] += dZ[img, row, col, ch]
+        if padding == "same":
+            dA[img, :, :, :] += dA_img[pad_h: -pad_h, pad_w: - pad_w]
+        if padding == "valid":
+            dA[img, :, :, :] += dA_img
+    return dA, dW, db
---
+++ supervised_learning/0x07-cnn/2-main.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import numpy as np
+conv_backward = __import__('2-conv_backward').conv_backward
+
+if __name__ == "__main__":
+    np.random.seed(0)
+    lib = np.load('../data/MNIST.npz')
+    X_train = lib['X_train']
+    _, h, w = X_train.shape
+    X_train_c = X_train[:10].reshape((-1, h, w, 1))
+
+    W = np.random.randn(3, 3, 1, 2)
+    b = np.random.randn(1, 1, 1, 2)
+
+    dZ = np.random.randn(10, h - 2, w - 2, 2)
+    print(conv_backward(dZ, X_train_c, W, b, padding="valid"))
