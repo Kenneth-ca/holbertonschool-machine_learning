62c9cbb7d863ae28d930853bbc6f5349da25ccb0
Kenneth-ca <kenneth.ca95@gmail.com>
2020-06-09 15:46:37 -0500

    add - lenet-5 with keras

---
+++ supervised_learning/0x07-cnn/5-lenet5.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+"""
+Builds a modified version of the LeNet-5 architecture using tensorflow
+"""
+import tensorflow.keras as K
+
+
+def lenet5(X):
+    """
+    a function that builds a modified LeNet-5 using tensorflow
+    :param X: tf.placeholder of shape (m, 28, 28, 1) containing the input
+    images for the network
+        m is the number of images
+    :return:
+        a tensor for the softmax activated output
+        a training operation that utilizes Adam optimization (with default
+        hyperparameters)
+        a tensor for the loss of the network
+        a tensor for the accuracy of the network
+    """
+    K.Model()
+    init = K.initializers.he_normal()
+    activation = "relu"
+
+    # 1st convolutional layer
+    conv1 = K.layers.Conv2D(filters=6, kernel_size=(5, 5), padding="same",
+                            activation=activation, kernel_initializer=init)(X)
+    pool1 = K.layers.MaxPooling2D(pool_size=[2, 2], strides=(2, 2))(conv1)
+
+    # 2nd convolutional layer
+    conv2 = K.layers.Conv2D(filters=6, kernel_size=(5, 5), padding="valid",
+                            activation=activation, kernel_initializer=init)(
+        pool1)
+    pool2 = K.layers.MaxPool2D(pool_size=[2, 2], strides=(2, 2))(conv2)
+
+    # Flatten
+    flatten = K.layers.Flatten()(pool2)
+
+    # Fully connected (FC) 1
+    fc1 = K.layers.Dense(units=120, activation=activation,
+                         kernel_initializer=init)(flatten)
+    # FC 2
+    fc2 = K.layers.Dense(units=84, activation=activation,
+                         kernel_initializer=init)(fc1)
+    # FC 3
+    fc3 = K.layers.Dense(units=10, activation="softmax",
+                         kernel_initializer=init)(fc2)
+
+    # Model
+    model = K.Model(inputs=X, outputs=fc3)
+
+    opt = K.optimizers.Adam()
+
+    model.compile(loss='categorical_crossentropy', optimizer=opt,
+                  metrics=['accuracy'])
+
+    return model
---
+++ supervised_learning/0x07-cnn/5-main.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+import matplotlib.pyplot as plt
+import numpy as np
+import tensorflow as tf
+import tensorflow.keras as K
+lenet5 = __import__('5-lenet5').lenet5
+
+if __name__ == "__main__":
+    np.random.seed(0)
+    tf.set_random_seed(0)
+    lib = np.load('../data/MNIST.npz')
+    X_train = lib['X_train']
+    m, h, w = X_train.shape
+    X_train_c = X_train.reshape((-1, h, w, 1))
+    Y_train = lib['Y_train']
+    Y_train_oh = K.utils.to_categorical(Y_train, num_classes=10)
+    X_valid = lib['X_valid']
+    X_valid_c = X_valid.reshape((-1, h, w, 1))
+    Y_valid = lib['Y_valid']
+    Y_valid_oh = K.utils.to_categorical(Y_valid, num_classes=10)
+
+    X = K.Input(shape=(h, w, 1))
+    model = lenet5(X)
+    batch_size = 32
+    epochs = 5
+    model.fit(X_train_c, Y_train_oh, batch_size=batch_size, epochs=epochs,
+                       validation_data=(X_valid_c, Y_valid_oh))
+
+    Y_pred = model.predict(X_valid_c)
+    print(Y_pred[0])
+    Y_pred = np.argmax(Y_pred, 1)
+    plt.imshow(X_valid[0])
+    plt.title(str(Y_valid[0]) + ' : ' + str(Y_pred[0]))
+    plt.show()
