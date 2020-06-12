e016ab58b3d2d87d262b84c34d9115fc72d766a8
Kenneth-ca <kenneth.ca95@gmail.com>
2020-06-12 11:24:35 -0500

    fix - filters number in second layer

--- supervised_learning/0x07-cnn/5-lenet5.py
+++ supervised_learning/0x07-cnn/5-lenet5.py
@@ -18,20 +18,19 @@ def lenet5(X):
         a tensor for the loss of the network
         a tensor for the accuracy of the network
     """
-    K.Model()
     init = K.initializers.he_normal()
     activation = "relu"
 
     # 1st convolutional layer
     conv1 = K.layers.Conv2D(filters=6, kernel_size=(5, 5), padding="same",
                             activation=activation, kernel_initializer=init)(X)
-    pool1 = K.layers.MaxPooling2D(pool_size=[2, 2], strides=(2, 2))(conv1)
+    pool1 = K.layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(conv1)
 
     # 2nd convolutional layer
-    conv2 = K.layers.Conv2D(filters=6, kernel_size=(5, 5), padding="valid",
+    conv2 = K.layers.Conv2D(filters=16, kernel_size=(5, 5), padding="valid",
                             activation=activation, kernel_initializer=init)(
         pool1)
-    pool2 = K.layers.MaxPool2D(pool_size=[2, 2], strides=(2, 2))(conv2)
+    pool2 = K.layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(conv2)
 
     # Flatten
     flatten = K.layers.Flatten()(pool2)
@@ -47,7 +46,7 @@ def lenet5(X):
                          kernel_initializer=init)(fc2)
 
     # Model
-    model = K.Model(inputs=X, outputs=fc3)
+    model = K.models.Model(inputs=X, outputs=fc3)
 
     opt = K.optimizers.Adam()
 
