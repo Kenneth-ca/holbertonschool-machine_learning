8bac7af18af942c1203a1c43dc6da17780c8362d
Kenneth-ca <kenneth.ca95@gmail.com>
2020-06-15 21:43:53 -0500

    fix - shape from max pool

--- supervised_learning/0x08-deep_cnns/1-inception_network.py
+++ supervised_learning/0x08-deep_cnns/1-inception_network.py
@@ -17,28 +17,25 @@ def inception_network():
                             kernel_initializer=init, activation="relu")(X)
 
     max_pool0 = K.layers.MaxPooling2D(pool_size=(3, 3), strides=(2, 2),
-                                      padding="valid")(conv0)
+                                      padding="same")(conv0)
 
-    conv1R = K.layers.Conv2D(64, kernel_size=3, strides=(2, 2), padding="valid",
+    conv1R = K.layers.Conv2D(64, kernel_size=1, strides=(1, 1), padding="same",
                              kernel_initializer=init, activation="relu")
     conv1R = conv1R(max_pool0)
 
-    conv1 = K.layers.Conv2D(192, kernel_size=3, strides=(2, 2), padding="same",
+    conv1 = K.layers.Conv2D(192, kernel_size=3, strides=(1, 1), padding="same",
                             kernel_initializer=init, activation="relu")
     conv1 = conv1(conv1R)
 
-    conv2 = K.layers.Conv2D(192, kernel_size=7, strides=(2, 2), padding="same",
-                            kernel_initializer=init, activation="relu")(conv1)
-
     max_pool1 = K.layers.MaxPooling2D(pool_size=(3, 3), strides=(2, 2),
-                                      padding="valid")(conv2)
+                                      padding="same")(conv1)
 
     incep0 = inception_block(max_pool1, [64, 96, 128, 16, 32, 32])
 
     incep1 = inception_block(incep0, [128, 128, 192, 32, 96, 64])
 
     max_pool2 = K.layers.MaxPooling2D(pool_size=(3, 3), strides=(2, 2),
-                                      padding="valid")(incep1)
+                                      padding="same")(incep1)
 
     incep2 = inception_block(max_pool2, [192, 96, 208, 16, 48, 64])
 
@@ -51,7 +48,7 @@ def inception_network():
     incep6 = inception_block(incep5, [256, 160, 320, 32, 128, 128])
 
     max_pool2 = K.layers.MaxPooling2D(pool_size=(3, 3), strides=(2, 2),
-                                      padding="valid")(incep6)
+                                      padding="same")(incep6)
 
     incep7 = inception_block(max_pool2, [256, 160, 320, 32, 128, 128])
 
