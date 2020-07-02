9e8b55525b9a085693d435894e479fc9dde4962a
Kenneth-ca <kenneth.ca95@gmail.com>
2020-07-02 17:15:42 -0500

    add - non-max suppression

---
+++ supervised_learning/0x0A-object_detection/3-main.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+if __name__ == '__main__':
+    import numpy as np
+    Yolo = __import__('3-yolo').Yolo
+
+    np.random.seed(0)
+    anchors = np.array([[[116, 90], [156, 198], [373, 326]],
+                        [[30, 61], [62, 45], [59, 119]],
+                        [[10, 13], [16, 30], [33, 23]]])
+    yolo = Yolo('../data/yolo.h5', '../data/coco_classes.txt', 0.6, 0.5, anchors)
+    output1 = np.random.randn(13, 13, 3, 85)
+    output2 = np.random.randn(26, 26, 3, 85)
+    output3 = np.random.randn(52, 52, 3, 85)
+    boxes, box_confidences, box_class_probs = yolo.process_outputs([output1, output2, output3], np.array([500, 700]))
+    boxes, box_classes, box_scores = yolo.filter_boxes(boxes, box_confidences, box_class_probs)
+    boxes, box_classes, box_scores = yolo.non_max_suppression(boxes, box_classes, box_scores)
+    print('Boxes:', boxes)
+    print('Box classes:', box_classes)
+    print('Box scores:', box_scores)
