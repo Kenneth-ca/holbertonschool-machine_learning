30af95b64ca40a2e5cf5b66b4be417cb14fd0054
Kenneth-ca <kenneth.ca95@gmail.com>
2020-07-22 17:21:26 -0500

    add - calculates the definiteness of a matrix

---
+++ math/0x05-advanced_linear_algebra/5-definiteness.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+"""
+Calculates the definiteness of a matrix
+"""
+import numpy as np
+
+
+def definiteness(matrix):
+    """
+    a function that calculates the definitiness of a matrix
+    :param matrix: a numpy.ndarray of shape (n, n) whose definiteness should
+    be calculated
+    :return: if the matrix is positive definite, positive semi-definite,
+    negative semi-definite, negative definite of indefinite, respectively
+    """
+    if type(matrix) is not np.ndarray:
+        raise TypeError("matrix must be a numpy.ndarray")
+    if len(matrix.shape) < 2:
+        return None
+    row, col = matrix.shape
+    if row != col:
+        return None
+    eigenvalues = np.linalg.eigvals(matrix)
+
+    pos = 0
+    neg = 0
+    semi = 0
+    for i in eigenvalues:
+        if i > 0:
+            pos = 1
+        if i < 0:
+            neg = 1
+        if i == 0:
+            semi = 1
+
+    if pos and not neg and not semi:
+        return "Positive definite"
+    elif pos and not neg and semi:
+        return "Positive semi-definite"
+    elif not pos and neg and not semi:
+        return "Negative definite"
+    elif not pos and neg and semi:
+        return "Negative semi-definite"
+    else:
+        return "Indefinite"
---
+++ math/0x05-advanced_linear_algebra/5-main.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+if __name__ == '__main__':
+    definiteness = __import__('5-definiteness').definiteness
+    import numpy as np
+
+    mat1 = np.array([[5, 1], [1, 1]])
+    mat2 = np.array([[2, 4], [4, 8]])
+    mat3 = np.array([[-1, 1], [1, -1]])
+    mat4 = np.array([[-2, 4], [4, -9]])
+    mat5 = np.array([[1, 2], [2, 1]])
+    mat6 = np.array([])
+    mat7 = np.array([[1, 2, 3], [4, 5, 6]])
+    mat8 = [[1, 2], [1, 2]]
+
+    print(definiteness(mat1))
+    print(definiteness(mat2))
+    print(definiteness(mat3))
+    print(definiteness(mat4))
+    print(definiteness(mat5))
+    print(definiteness(mat6))
+    print(definiteness(mat7))
+    try:
+        definiteness(mat8)
+    except Exception as e:
+        print(e)
