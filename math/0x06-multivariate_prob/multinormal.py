1abb9e6ae37c66531abfad621c25fba1365ccdea
Kenneth-ca <kenneth.ca95@gmail.com>
2020-07-27 15:48:16 -0500

    fix - condition valuerror

--- math/0x06-multivariate_prob/multinormal.py
+++ math/0x06-multivariate_prob/multinormal.py
@@ -41,7 +41,7 @@ class MultiNormal:
             raise TypeError("x must be a numpy.ndarray")
         d = self.cov.shape[0]
         if len(x.shape) != 2 or x.shape[0] != d or x.shape[1] != 1:
-            raise TypeError("x must have the shape ({}, 1)".format(d))
+            raise ValueError("x must have the shape ({}, 1)".format(d))
 
         det = np.linalg.det(self.cov)
         inv = np.linalg.inv(self.cov)
