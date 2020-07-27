392b19a22b641ea680f441ee6b72340fef905536
Kenneth-ca <kenneth.ca95@gmail.com>
2020-07-27 13:21:49 -0500

    add - calculates the covariance

---
+++ math/0x06-multivariate_prob/0-mean_cov.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""
+Calculates the mean and covariance of a data set
+"""
+import numpy as np
+
+
+def mean_cov(X):
+    """
+    a function that calculates the mean and covariance
+    :param X: numpy.ndarray of shape (n, d) containing the data set:
+        n is the number of data points
+        d is the number of dimensions in each data point
+        If X is not a 2D numpy.ndarray, raise a TypeError with the message X
+        must be a 2D numpy.ndarray
+        If n is less than 2, raise a ValueError with the message X must contain
+        multiple data points
+    :return:
+    """
+    if type(X) is not np.ndarray:
+        raise TypeError("X must be a 2D numpy.ndarray")
+    if len(X.shape) != 2:
+        raise ValueError("X must contain multiple data points")
+    n, d = X.shape
+
+    mean = X.mean(axis=0).reshape(1, d)
+    X_mean = X - mean
+    cov = (X_mean.T @ X_mean) / (n - 1)
+    return mean, cov
