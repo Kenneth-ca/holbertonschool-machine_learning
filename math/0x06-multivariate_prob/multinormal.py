2784e94ca0395798d0454c31373530d91d34ae4b
Kenneth-ca <kenneth.ca95@gmail.com>
2020-07-27 16:08:47 -0500

    feat - testing other options

--- math/0x06-multivariate_prob/multinormal.py
+++ math/0x06-multivariate_prob/multinormal.py
@@ -40,7 +40,7 @@ class MultiNormal:
         if type(x) is not np.ndarray:
             raise TypeError("x must be a numpy.ndarray")
         d = self.cov.shape[0]
-        if len(x.shape) != 2 or x.shape[0] != d or x.shape[1] != 1:
+        if len(x.shape) != 2 or x.shape[1] != 1 or x.shape[0] != d:
             raise ValueError("x must have the shape ({}, 1)".format(d))
 
         det = np.linalg.det(self.cov)
