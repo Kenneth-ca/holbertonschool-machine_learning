5e3a94eb11bb93a3c1295fe7df28fb912232b4af
Kenneth-ca <kenneth.ca95@gmail.com>
2020-07-29 09:38:32 -0500

    feat - changed name of variables

--- unsupervised_learning/0x00-dimensionality_reduction/4-P_affinities.py
+++ unsupervised_learning/0x00-dimensionality_reduction/4-P_affinities.py
@@ -29,23 +29,23 @@ def P_affinities(X, tol=1e-5, perplexity=30.0):
         Hi, Pi = HP(copy, betas[i, 0])
         Hdiff = Hi - H
 
-        betamin = None
-        betamax = None
+        low = None
+        high = None
 
         while np.abs(Hdiff) > tol:
             if Hdiff > 0:
-                betamin = betas[i, 0]
-                if betamax is None:
+                low = betas[i, 0]
+                if high is None:
                     betas[i, 0] = betas[i, 0] * 2
                 else:
-                    betas[i, 0] = (betas[i, 0] + betamax) / 2
+                    betas[i, 0] = (betas[i, 0] + high) / 2
 
             else:
-                betamax = betas[i, 0]
-                if betamin is None:
+                high = betas[i, 0]
+                if low is None:
                     betas[i, 0] = betas[i, 0] / 2
                 else:
-                    betas[i, 0] = (betas[i, 0] + betamin) / 2
+                    betas[i, 0] = (betas[i, 0] + low) / 2
 
             Hi, Pi = HP(copy, betas[i, 0])
             Hdiff = Hi - H
