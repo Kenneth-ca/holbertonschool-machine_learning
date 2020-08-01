29d98fe9e002ba1e44896db80890b789a105c0b3
Kenneth-ca <kenneth.ca95@gmail.com>
2020-07-31 23:44:31 -0500

    test - conditions

--- math/0x07-bayesian_prob/100-continuous.py
+++ math/0x07-bayesian_prob/100-continuous.py
@@ -24,9 +24,9 @@ def posterior(x, n, p1, p2):
     if x > n:
         raise ValueError("x cannot be greater than n")
 
-    if type(p1) is not float or not 0 <= p1 <= 1:
+    if type(p1) is not float or p1 <= 0 or p1 >= 1:
         raise ValueError("p1 must be a float in the range [0, 1]")
-    if type(p2) is not float or not 0 <= p2 <= 1:
+    if type(p2) is not float or p2 <= 0 or p2 >= 1:
         raise ValueError("p2 must be a float in the range [0, 1]")
 
     if p2 <= p1:
