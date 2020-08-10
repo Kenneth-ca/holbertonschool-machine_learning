ecaf49ee1dca7fa2e72ba36f7259b08af22d583f
Kenneth-ca <kenneth.ca95@gmail.com>
2020-08-10 15:27:39 -0500

    add - steady state probabilities of a regular markov chain

---
+++ unsupervised_learning/0x02-hmm/1-regular.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""
+Determines the steady state probabilities of a regular markov chain
+"""
+import numpy as np
+
+
+def regular(P):
+    """
+
+    :param P:
+    :return:
+    """
+    if type(P) is not np.ndarray:
+        return None
+    if len(P.shape) != 2:
+        return None
+    n, n_t = P.shape
+    if n != n_t:
+        return None
+    sum_test = np.sum(P, axis=1)
+    for elem in sum_test:
+        if not np.isclose(elem, 1):
+            return None
+
+    evals, evecs = np.linalg.eig(P.T)
+    stationary = evecs / evecs.sum()
+    stationary = stationary.real
+
+    for i in np.dot(stationary.T, P):
+        if (i >= 0).all() and np.isclose(i.sum(), 1):
+            return i.reshape(1, n)
+
+    return None
