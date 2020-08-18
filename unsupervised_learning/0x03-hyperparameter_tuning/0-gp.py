df327328e6bbc2c1f85d5709237add70764d828f
Kenneth-ca <kenneth.ca95@gmail.com>
2020-08-18 09:21:02 -0500

    add - initilization of class gaussian process

---
+++ unsupervised_learning/0x03-hyperparameter_tuning/0-gp.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+"""
+Create the class GaussianProcess that represents a noiseless 1D
+Gaussian process
+"""
+import numpy as np
+
+
+class GaussianProcess:
+    """
+    A class that represents a Gaussian process
+    """
+
+    def __init__(self, X_init, Y_init, l=1, sigma_f=1):
+        """
+        A function that initializes a class
+        :param X_init: numpy.ndarray of shape (t, 1) representing the inputs
+        already sampled with the black-box function
+        :param Y_init: numpy.ndarray of shape (t, 1) representing the outputs
+        of the black-box function for each input in X_init
+        :param l: the length parameter for the kernel
+        :param sigma_f: the standard deviation given to the output of the
+        black-box function
+        """
+        self.X = X_init
+        self.Y = Y_init
+        self.l = l
+        self.sigma_f = sigma_f
+        self.K = self.kernel(self.X, self.X)
+
+    def kernel(self, X1, X2):
+        """
+        Calculates the covariance kernel matrix between two matrices
+        :param X1: numpy.ndarray of shape (m, 1)
+        :param X2: numpy.ndarray of shape (n, 1)
+        :return: the covariance kernel matrix as a numpy.ndarray of
+        shape (m, n)
+        """
+        sqdist = (np.sum(X1 ** 2, 1).reshape(-1, 1) + np.sum(X2 ** 2, 1)
+                  - 2 * np.dot(X1, X2.T))
+        return self.sigma_f ** 2 * np.exp(-0.5 / self.l ** 2 * sqdist)
---
+++ unsupervised_learning/0x03-hyperparameter_tuning/0-main.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+GP = __import__('0-gp').GaussianProcess
+import numpy as np
+
+
+def f(x):
+    """our 'black box' function"""
+    return np.sin(5*x) + 2*np.sin(-2*x)
+
+if __name__ == '__main__':
+    np.random.seed(0)
+    X_init = np.random.uniform(-np.pi, 2*np.pi, (2, 1))
+    Y_init = f(X_init)
+
+    gp = GP(X_init, Y_init, l=0.6, sigma_f=2)
+    print(gp.X is X_init)
+    print(gp.Y is Y_init)
+    print(gp.l)
+    print(gp.sigma_f)
+    print(gp.K.shape, gp.K)
+    print(np.allclose(gp.kernel(X_init, X_init), gp.K))
