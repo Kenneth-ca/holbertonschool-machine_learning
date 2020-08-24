beeafa700ec0d59683e481ba724ddfcc2a4bdf01
Kenneth-ca <kenneth.ca95@gmail.com>
2020-08-24 14:00:12 -0500

    add - vanilla autoencoder

---
+++ unsupervised_learning/0x04-autoencoders/0-main.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+import matplotlib.pyplot as plt
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.datasets import mnist
+
+autoencoder = __import__('0-vanilla').autoencoder
+
+(x_train, _), (x_test, _) = mnist.load_data()
+x_train = x_train.astype('float32') / 255.
+x_test = x_test.astype('float32') / 255.
+x_train = x_train.reshape((-1, 784))
+x_test = x_test.reshape((-1, 784))
+np.random.seed(0)
+tf.set_random_seed(0)
+encoder, decoder, auto = autoencoder(784, [128, 64], 32)
+auto.fit(x_train, x_train, epochs=50,batch_size=256, shuffle=True,
+                validation_data=(x_test, x_test))
+encoded = encoder.predict(x_test[:10])
+print(np.mean(encoded))
+reconstructed = decoder.predict(encoded)
+
+for i in range(10):
+    ax = plt.subplot(2, 10, i + 1)
+    ax.axis('off')
+    plt.imshow(x_test[i].reshape((28, 28)))
+    ax = plt.subplot(2, 10, i + 11)
+    ax.axis('off')
+    plt.imshow(reconstructed[i].reshape((28, 28)))
+plt.show()
