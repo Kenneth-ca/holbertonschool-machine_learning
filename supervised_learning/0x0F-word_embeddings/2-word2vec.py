f741c2ea00b791c7a4a5144fab82af1675c9fd6d
Kenneth-ca <kenneth.ca95@gmail.com>
2020-09-15 11:00:27 -0500

    add - creates and train gensim word2vec model

---
+++ supervised_learning/0x0F-word_embeddings/2-word2vec.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""
+Creates and trains a gensim word2vec model
+"""
+from gensim.models import Word2Vec
+import tensorflow.keras
+
+
+def word2vec_model(sentences, size=100, min_count=5, window=5, negative=5,
+                   cbow=True, iterations=5, seed=0, workers=1):
+    """
+    Creates and trains a gensim word2vec model
+    :param sentences: a list of sentences to be trained on
+    :param size: the dimensionality of the embedding layer
+    :param min_count: the minimum number of occurrences of a word for use in
+    training
+    :param window: the maximum distance between the current and predicted
+    word within a sentence
+    :param negative: the size of negative sampling
+    :param cbow: boolean to determine the training type; True is for CBOW;
+    False is for Skip-gram
+    :param iterations: the size of negative sampling
+    :param seed: the seed for the random number generator
+    :param workers: the number of worker threads to train the model
+    :return: the trained model
+    """
+    if cbow is True:
+        skip = 0
+    else:
+        skip = 1
+    model = Word2Vec(size=size, window=window,
+                     min_count=min_count, workers=workers, sg=skip,
+                     negative=negative, seed=seed)
+    model.build_vocab(sentences)
+    model.train(sentences, total_examples=model.corpus_count, epochs=iterations)
+    return model
