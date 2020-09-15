117a0cc3d31c1ac659a5a01c922a6ee2d6664b53
Kenneth-ca <kenneth.ca95@gmail.com>
2020-09-15 11:18:28 -0500

    add - convert gensim to keras

---
+++ supervised_learning/0x0F-word_embeddings/3-gensim_to_keras.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Converts a gensim word2vec model to a keras Embedding layer
+"""
+from gensim.models import Word2Vec
+import tensorflow.keras as keras
+
+
+def gensim_to_keras(model):
+    """
+    Converts a gensim word2vec model to a keras Embedding layer
+    :param model:
+    :return:
+    """
+    # return model.wv.get_keras_embedding(train_embeddings=False)
