e9f84bb8b4d3386d8bfa080589c9aae42f10b479
Kenneth-ca <kenneth.ca95@gmail.com>
2020-10-26 12:04:20 -0500

    add - converts to numpy

---
+++ pipeline/0x00-pandas/4-array.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+import pandas as pd
+from_file = __import__('2-from_file').from_file
+
+df = from_file('coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv', ',')
+
+A = df[['High', 'Close']].tail(10).to_numpy()
+
+print(A)
