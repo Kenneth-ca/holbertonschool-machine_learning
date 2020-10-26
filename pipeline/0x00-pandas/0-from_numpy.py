f149c575ee5da3006b98d946e7426c9bda14ed0f
Kenneth-ca <kenneth.ca95@gmail.com>
2020-10-26 10:34:41 -0500

    add - converts a numpy array into a dataframe and labels it

---
+++ pipeline/0x00-pandas/0-from_numpy.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Creates a pd.DataFrame from a np.ndarray
+"""
+import pandas as pd
+
+def from_numpy(array):
+    """
+    Creates a dataframe
+    :param array: the np.ndarray from which you should create the pd.DataFrame
+    :return: the newly created pd.DataFrame
+    """
+    df = pd.DataFrame(array)
+    _, n = array.shape
+    alphabet = range(65, 65 + n)
+    alphabet = list(map(lambda x: chr(x), alphabet))
+    df.columns = alphabet
+    return df
