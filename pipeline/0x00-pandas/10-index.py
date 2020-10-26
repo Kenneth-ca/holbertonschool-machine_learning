2454d8418ed95f14d80954553e9f341ad8a8fbfa
Kenneth-ca <kenneth.ca95@gmail.com>
2020-10-26 14:37:40 -0500

    add - set index

---
+++ pipeline/0x00-pandas/10-index.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+import pandas as pd
+from_file = __import__('2-from_file').from_file
+
+df = from_file('coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv', ',')
+
+df = df.set_index('Timestamp')
+
+print(df.tail())
