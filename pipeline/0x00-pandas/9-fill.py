186d5550e4cf907965d2329fa24a0258a81c84e4
Kenneth-ca <kenneth.ca95@gmail.com>
2020-10-27 10:02:43 -0500

    fix - drop weighted price

--- pipeline/0x00-pandas/9-fill.py
+++ pipeline/0x00-pandas/9-fill.py
@@ -5,6 +5,7 @@ from_file = __import__('2-from_file').from_file
 
 df = from_file('coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv', ',')
 
+df.drop('Weighted_Price', axis=1, inplace=True)
 df['Close'].fillna(method='ffill', inplace=True)
 df["Volume_(BTC)"].fillna(value=0, inplace=True)
 df["Volume_(Currency)"].fillna(value=0, inplace=True)
