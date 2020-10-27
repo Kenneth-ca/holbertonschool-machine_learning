5ee513d3a7967688c66adc098ce355991f8f2602
Kenneth-ca <kenneth.ca95@gmail.com>
2020-10-26 22:30:44 -0500

    fix - fill with close and fill volumes with 0

--- pipeline/0x00-pandas/9-fill.py
+++ pipeline/0x00-pandas/9-fill.py
@@ -5,7 +5,13 @@ from_file = __import__('2-from_file').from_file
 
 df = from_file('coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv', ',')
 
-df.fillna(method='bfill', inplace=True)
+df['Close'].fillna(method='ffill', inplace=True)
+df["Volume_(BTC)"].fillna(value=0, inplace=True)
+df["Volume_(Currency)"].fillna(value=0, inplace=True)
+
+df = df.fillna({'Open': df['Close'].shift(1, fill_value=0),
+                'High': df['Close'].shift(1, fill_value=0),
+                'Low': df['Close'].shift(1, fill_value=0)})
 
 print(df.head())
 print(df.tail())
