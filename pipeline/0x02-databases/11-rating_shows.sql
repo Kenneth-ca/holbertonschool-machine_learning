2a526edece2f16fe24e3895140daaa7ffe97835b
Kenneth-ca <kenneth.ca95@gmail.com>
2020-11-02 21:35:35 +0000

    test - no alias

--- pipeline/0x02-databases/11-rating_shows.sql
+++ pipeline/0x02-databases/11-rating_shows.sql
@@ -1,8 +1,8 @@
 -- Rotten tomatoes
-SELECT s.title, SUM(r.rate) AS rating
-FROM tv_show_ratings as r
-JOIN tv_shows AS s
-ON r.show_id = s.id
-GROUP BY s.title
+SELECT tv_shows.title, SUM(tv_show_ratings.rate) AS rating
+FROM tv_show_ratings
+JOIN tv_shows
+ON tv_show_ratings.show_id = tv_shows.id
+GROUP BY tv_shows.title
 ORDER BY rating DESC
 ;
