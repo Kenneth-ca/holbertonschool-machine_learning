ac0431d8ad62b8af7711bd36b6793b8dc0636ab5
Kenneth-ca <kenneth.ca95@gmail.com>
2020-11-03 02:53:59 +0000

    add - create table with enumeration

---
+++ pipeline/0x02-databases/14-country_users.sql
@@ -0,0 +1,8 @@
+-- Creates a table user
+CREATE TABLE IF NOT EXISTS users (
+	id INT NOT NULL AUTO_INCREMENT,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	name VARCHAR(255),
+	country ENUM('US', 'CO', 'TN') NOT NULL,
+	PRIMARY KEY (id)
+);
